7a219c7644931f9225f558021f2cafbe1c2336a8
Damian Lluch <damianclluch@gmail.com>
2018-08-07 22:28:51 +0200

    fix

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,26 +9,35 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient ) {
 
     // console.log('Servicio de infoPagina listo');
-
-    // Leer el archivo JSON
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+  private cargarInfo() {
     this.http.get('assets/data/data-pagina.json')
         .subscribe( (resp: InfoPagina) => {
 
           this.cargada = true;
           this.info = resp;
-          console.log(resp);
-
-
         });
+  }
 
+  private cargarEquipo() {
+    this.http.get('https://acheestudio-d4d45.firebaseio.com/equipo.json')
+    .subscribe( ( resp: any[]) => {
 
+      this.equipo = resp;
+      console.log(resp);
+    });
   }
 
 }
+
+
 
 
 
