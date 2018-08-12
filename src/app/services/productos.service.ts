4e9f15153f95ea987741956e8ce2f01c46bbf886
Damian Lluch <damianclluch@gmail.com>
2018-08-12 15:34:33 +0200

    fix

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -8,6 +8,7 @@ import { Producto } from '../interfaces/producto.interface';
 export class ProductosService {
 
   cargando = true;
+  productos: Producto[] = [];
 
   constructor(private http: HttpClient) {
     this.cargarProductos();
@@ -17,6 +18,7 @@ export class ProductosService {
     this.http.get('https://acheestudio-d4d45.firebaseio.com/productos_idx.json')
       .subscribe( (resp: Producto[]) => {
         console.log(resp);
+        this.productos = resp;
         this.cargando = false;
       });
   }
